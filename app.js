303941fac9e6f79f8ba7493e34ba7787e0247d48
Francisco M. Miranda <fmanuelos@gmail.com>
2020-06-24 14:36:03 -0400

    Update node port 3000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var static = require('node-static');
 // Create a node-static server instance to serve the './public' folder
 //
 var file = new static.Server('./dist');
-var port  = 8000
+var port  = 3000;
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
