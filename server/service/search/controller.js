3bebff2f69be6d5c4938ba9728be76f5cf5cadf2
Yu <yuw5@nih.gov>
2020-09-01 10:05:26 -0400

    Added CTDC indexing algorithm

--- server/service/search/controller.js
+++ server/service/search/controller.js
@@ -11,163 +11,10 @@ const xlsx = require('node-xlsx');
 const _ = require('lodash');
 const shared = require('./shared');
 // const git = require('nodegit');
+const dataFilesPath = path.join(__dirname, '..', '..', 'data_files');
 var cdeData = {};
 var gdcData = {};
-
-const indexing_old = (req, res) => {
-	let configs = [];
-	//config property index
-	let config_property = {};
-	config_property.index = config.index_p;
-	config_property.body = {
-		"settings": {
-			"number_of_shards": 10, 
-			"max_inner_result_window": 10000000,
-			"max_result_window": 10000000,
-			"analysis": {
-				"analyzer": {
-					"case_insensitive": {
-						"tokenizer": "keyword",
-						"filter": ["lowercase", "whitespace_remove"]
-					},
-					"my_standard": {
-						"tokenizer": "standard",
-						"char_filter": ["my_filter"],
-						"filter": ["lowercase","whitespace_remove"]
-					},
-					"my_ngram": {
-						"tokenizer": "ngram_tokenizer",
-						"char_filter": ["my_filter"],
-						"filter": ["lowercase","whitespace_remove"]
-					}
-				},
-				"char_filter": {
-					"my_filter": {
-						"type": "mapping",
-						"mappings": ["_=>-"]
-					}
-				},
-				"filter": {
-					"whitespace_remove": {
-						"type": "pattern_replace",
-						"pattern": "[_-]",
-						"replacement": " "
-					}
-				},
-				"tokenizer": {
-					"ngram_tokenizer": {
-						"type": "nGram",
-						"min_gram": "2",
-						"token_chars": ["letter", "digit", "symbol"]
-					}
-				}
-			}
-		},
-		"mappings": {
-				"properties": {
-					"id": {
-						"type": "keyword"
-					},
-					"category": {
-						"type": "keyword"
-					},
-					"node": {
-						"type": "keyword"
-					},
-					"property": {
-						"type": "text",
-						"fields": {
-							"have": {
-								"type": "text",
-								"analyzer": "my_standard"
-							}
-						},
-						"analyzer": "case_insensitive"
-					},
-					"enum":{
-						"type": "nested",
-						"properties": {
-							"n": {
-								"type": "text",
-								"fields": {
-									"have": {
-										"type": "text",
-										"analyzer": "my_standard"
-									}
-								},
-								"analyzer": "case_insensitive"
-							},
-							"n_syn.s.termName": {
-								"type": "text",
-								"fields": {
-									"have": {
-										"type": "text",
-										"analyzer": "my_standard"
-									}
-								},
-								"analyzer": "case_insensitive"
-							},
-							"n_syn.n_c": {
-								"type": "text",
-								"fields": {
-									"have": {
-										"type": "text",
-										"analyzer": "my_standard"
-									}
-								},
-								"analyzer": "case_insensitive"
-							},
-							"i_c":{
-								"properties": {
-									"c": {
-										"type": "text",
-										"analyzer": "case_insensitive"
-									},
-									"have": {
-										"type": "text",
-										"analyzer": "my_standard"
-									}
-								}
-							}
-						}
-					},
-					"cde.id": {
-						"type": "text",
-						"analyzer": "case_insensitive"
-					}
-				}
-		}
-	};
-	configs.push(config_property);
-	//config suggestion index
-	let config_suggestion = {};
-	config_suggestion.index = config.suggestionName;
-	config_suggestion.body = {
-		"mappings": {
-				"properties": {
-					"id": {
-						"type": "completion",
-						"max_input_length": 100,
-						"analyzer": "standard",
-						"search_analyzer": "standard",
-						"preserve_separators": false
-					}
-				}
-		}
-	};
-	configs.push(config_suggestion);
-	elastic.createIndexes(configs, result => {
-		if (result.acknowledged === undefined) {
-			return handleError.error(res, result);
-		}
-		elastic.bulkIndex_old(data => {
-			if (data.property_indexed === undefined) {
-				return handleError.error(res, data);
-			}
-			return res.status(200).json(data);
-		});
-	});
-};
+var syns = {};
 
 const indexing = (req, res) => {
 	let configs = [];
@@ -444,8 +291,92 @@ const getValuesForGraphicalView = async function(req, res) {
 	res.json(result);
 }
 
+const synchronziedLoadSynonmysfromNCIT = (ncitids, idx, next) => {
+	if (idx >= ncitids.length) {
+		return;
+	}
+	let syn = [];
+	https.get(config.NCIt_url[4] + ncitids[idx], (rsp) => {
+		let html = '';
+		rsp.on('data', (dt) => {
+			html += dt;
+		});
+		rsp.on('end', () => {
+			if (html.trim() !== '') {
+				let d = JSON.parse(html);
+				if (d.synonyms !== undefined) {
+					let tmp = {}
+					tmp[ncitids[idx]] = {};
+					tmp[ncitids[idx]].preferredName = d.preferredName;
+					tmp[ncitids[idx]].code = d.code;
+					tmp[ncitids[idx]].definitions = d.definitions;
+					tmp[ncitids[idx]].synonyms = [];
+					let checker_arr = [];
+					d.synonyms.forEach(data => {
+						if(checker_arr.indexOf((data.termName+"@#$"+data.termGroup+"@#$"+data.termSource).trim().toLowerCase()) !== -1) return;
+						let obj = {};
+						obj.termName = data.termName;
+						obj.termGroup = data.termGroup;
+						obj.termSource = data.termSource;
+						tmp[ncitids[idx]].synonyms.push(obj);
+						checker_arr.push((data.termName+"@#$"+data.termGroup+"@#$"+data.termSource).trim().toLowerCase());
+					});
+					if (d.additionalProperties !== undefined) {
+						tmp[ncitids[idx]].additionalProperties = [];
+						d.additionalProperties.forEach(data => {
+							let obj = {};
+							obj.name = data.name;
+							obj.value = data.value;
+							tmp[ncitids[idx]].additionalProperties.push(obj);
+						});
+					}
+					let str = {};
+					str[ncitids[idx]] = syns;
+					fs.appendFile(dataFilesPath + "/GDC/ncit_details.js", JSON.stringify(tmp), err => {
+						if (err) return logger.error(err);
+					});
+				} else {
+					logger.debug("!!!!!!!!!!!! no synonyms for " + ncitids[idx]);
+				}
+
+			}
+			syns[ncitids[idx]] = syn;
+			idx++;
+			synchronziedLoadSynonmysfromNCIT(ncitids, idx, next);
+			if (ncitids.length == idx) {
+				return next('Success');
+			} else {
+				return next("NCIT finished number: " + idx + " of " + ncitids.length + "\n");
+			}
+
+		});
+
+	}).on('error', (e) => {
+		logger.debug(e);
+	});
+};
+
+const preloadNCItSynonyms = (req, res) => {
+	let unloaded_ncits = cache.getValue("unloaded_ncits");
+	if(unloaded_ncits && unloaded_ncits.length > 0){
+		console.log(unloaded_ncits);
+		synchronziedLoadSynonmysfromNCIT(unloaded_ncits, 0, data => {
+			if (data === "Success") {
+				unloaded_ncits = [];
+				res.end('Success!!');
+			} else {
+				res.write(data);
+			}
+		});
+	}
+	else{
+		res.end('Success with no data processed!!');
+	}
+	
+	
+};
+
 module.exports = {
-	indexing_old,
 	indexing,
 	suggestion,
 	searchP,
@@ -453,5 +384,6 @@ module.exports = {
 	getGraphicalGDCDictionary,
 	getGraphicalICDCDictionary,
 	getGraphicalCTDCDictionary,
-	getValuesForGraphicalView
+	getValuesForGraphicalView,
+	preloadNCItSynonyms
 };
