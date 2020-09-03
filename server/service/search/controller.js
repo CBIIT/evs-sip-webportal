d1f02974729ae05ebf795f60eb25fbc08eee5ca1
Yu <yuw5@nih.gov>
2020-09-03 10:52:35 -0400

    Reload synonyms for missing ncit codes

--- server/service/search/controller.js
+++ server/service/search/controller.js
@@ -364,7 +364,6 @@ const synchronziedLoadSynonmysfromNCIT = (ncitids, idx, next) => {
 const preloadNCItSynonyms = (req, res) => {
 	let unloaded_ncits = cache.getValue("unloaded_ncits");
 	if(unloaded_ncits && unloaded_ncits.length > 0){
-		console.log(unloaded_ncits);
 		synchronziedLoadSynonmysfromNCIT(unloaded_ncits, 0, data => {
 			if (data === "Success") {
 				unloaded_ncits = [];
