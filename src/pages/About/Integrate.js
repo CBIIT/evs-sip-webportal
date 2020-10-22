62a10c1f896ca1841b21de08e67383fed8f113e1
Francisco M. Miranda <fmanuelos@gmail.com>
2020-10-22 10:50:13 -0400

    Update message for how to integrate into EVS-SIP

--- src/pages/About/Integrate.js
+++ src/pages/About/Integrate.js
@@ -33,7 +33,7 @@ const ContentBoxTitle = styled.h2`
 
 const ContentBoxText = styled.div`
   margin-top: 2rem;
-  max-width: 34rem;
+  max-width: 44rem;
 
   && > p {
     font-size: 1.0625rem;
@@ -52,24 +52,27 @@ const ContentBoxText = styled.div`
 
 const Integrate = () => {
   return <ContentBoxIntegrate>
-    <ContentBoxTitle>How to integrate into EVS-SIP</ContentBoxTitle>
+    <ContentBoxTitle>EVS-SIP Data Integration Process</ContentBoxTitle>
     <ContentBoxText>
+      <p>EVS-SIP is an open platform and can integrate data models from any sources into the platform. Please execute the following steps:</p>  
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat:
+      <b>Step 1: Data model preparation</b><br/>
+        To establish a data model for EVS-SIP, we recommend use of the Metadata Description Framework (MDF) tool. 
+        For details on how to setup a data model, please see: <a href="https://github.com/CBIIT/bento-mdf/tree/master/setup" target="_blank" rel="noopener noreferrer">https://github.com/CBIIT/bento-mdf/tree/master/setup</a>. 
+        For specifications on defining properties, values and relationships of a data model, please refer to: <a href="https://github.com/CBIIT/bento-mdf#model-description-files-mdf" target="_blank" rel="noopener noreferrer">https://github.com/CBIIT/bento-mdf#model-description-files-mdf</a>.
       </p>
-      <ul>
-        <li>Duis aute irure dolor in reprehenderit in voluptate velit cesse cillum dolore eu</li>
-        <li>Duis aute irure dolor in reprehenderit in voluptate velit cesse cillum dolore eu</li>
-      </ul>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-        qui officia deserunt mollit anim id est laborum.
+        <b>Step 2: EVS-SIP in-take process and continuous data model update</b><br/>
+        The initial EVS-SIP in-take process can begin with an unmapped MDF data model or with a model that has been partially 
+        or fully mapped to standardized vocabularies (see step 3 for details) or harmonized with pre-existing models found in EVS-SIP. 
+        The EVS-SIP platform supports on-going model changes and mapping updates. 
+        All modifications will be promoted to production tier on a regular cycle.
+      </p>
+      <p>
+        <b>Step 3: Data Model Mapping</b><br/>
+        Model atoms (i.e. properties, permissible values, etc.) can be mapped to the NCI Thesaurus (NCIt) and integrated vocabularies. 
+        Mapping of all data model atoms to NCIt or ICD-O-3 codes is a manual process performed by terminology experts. 
+        Please contact EVS team @ <a href="mailto:ncithesaurus@mail.nih.gov">ncithesaurus@mail.nih.gov</a> with questions or for assistance.
       </p>
     </ContentBoxText>
   </ContentBoxIntegrate>
