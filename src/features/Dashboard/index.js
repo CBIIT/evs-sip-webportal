ea4ce772be910f211bf3869d086ff6b269f1d1eb
Francisco M. Miranda <fmanuelos@gmail.com>
2021-09-20 15:42:15 -0400

    Update Dashboard Menu propotype

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { Container, Nav, NavDropdown, Row, Button, Col, Table, Pagination, InputGroup, FormControl} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import bkgd from '../../assets/img/dash-bkgd.jpg';
-
-import { Container, Row, Button, ListGroup, Col, Table, Pagination } from 'react-bootstrap';
 
 
 const Page = styled.div`
@@ -38,11 +39,11 @@ const ContainerStyled = styled(Container)`
   padding: 2rem;
 `;
 
-const ListGroupItemStyled = styled(ListGroup.Item)`
-  border-top: 1px solid rgba(0,0,0,.125) !important;
-  border-right: solid #176bd3 5px !important;
-  margin-bottom: 3px;
-`;
+// const ListGroupItemStyled = styled(ListGroup.Item)`
+//   border-top: 1px solid rgba(0,0,0,.125) !important;
+//   border-right: solid #176bd3 5px !important;
+//   margin-bottom: 3px;
+// `;
 
 const SectionHeader = styled.div`
   width: 100%;
@@ -99,21 +100,123 @@ const ButtonStyled = styled(Button)`
 const ButtonBlue = styled(ButtonStyled)`
   background-color: #6fc0d9;
   border-color: #34859d;
+
+  &&:hover,
+  &&:focus {
+    background-color: #5b9baf;
+  }
 `;
 
 const ButtonGray = styled(ButtonStyled)`
   background-color: #858d8f;
   border-color: #424242;
+
+  &&:hover,
+  &&:focus {
+    background-color: #63696b;
+  }
 `;
 
 const ButtonGreen = styled(ButtonStyled)`
   background-color: #00e097;
   border-color: #0a8867;
+
+  &&:hover,
+  &&:focus {
+    background-color: #00a770;
+  }
 `;
 
 const ButtonDarkBlue = styled(ButtonStyled)`
   background-color: #34859d;
   border-color: #34859d;
+
+  &&:hover,
+  &&:focus {
+    background-color: #245b6b;
+  }
+`;
+
+const InputGroupStyled = styled(InputGroup)`
+  margin-bottom: 1rem;
+  max-width: 20rem;
+`;
+
+const InputGroupTextStyled = styled(InputGroup.Text)`
+    position: relative;
+    left: -2.5rem;
+    z-index: 3;
+    background-color: transparent;
+    border: none;
+
+    &&>.form-control {
+      border-radius: .25rem;
+    }
+`;
+
+const FormControlStyled = styled(FormControl)`
+  border-radius: 1rem !important;
+  padding-right: 2rem;
+`;
+
+const NavLinkStyled = styled(Nav.Link)`
+  padding: .75rem 1.25rem;
+  border: 1px solid rgba(0,0,0,.125);
+  border-right: solid #176bd3 5px;
+  margin-bottom: 3px;
+  color: var(--black);
+`;
+
+const NavDropdownStyled = styled(NavDropdown)`
+  margin-bottom: 3px;
+
+  && > .dropdown-toggle {
+    padding: .75rem 1.25rem;
+    border: 1px solid rgba(0,0,0,.125);
+    border-right: solid #176bd3 5px;
+    color: var(--black);
+  }
+
+  && > .dropdown-menu {
+    padding: 0;
+    border: none;
+    width: 100%;
+    border: 1px solid rgba(0,0,0,.125);
+    border-top: 1px solid transparent;
+  }
+  
+
+  && > .dropdown-menu.show {
+    position: static !important;
+    transform: none !important;
+  }
+`;
+
+const NavDropdownItemStyled = styled(NavDropdown.Item)`
+  padding: .75rem 1.25rem;
+  text-align: center;
+  background-color: #eaf1f8;
+  color: #6a6c6f;
+
+  &&::after {
+    content: "";
+    border: 1px solid #d1d1d1;
+    display: block;
+    width: 9rem;
+    position: relative;
+    top: 0.6rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &&:last-child:after{
+    border: 1px solid transparent;
+  }
+`;
+
+const InputGroupIcon = styled(FontAwesomeIcon)`
+  font-size: 1rem;
+  vertical-align: 0;
 `;
 
 
@@ -132,13 +235,31 @@ const Dashboard = (props) => {
       <ContainerStyled>
         <Row>
         <Col sm={3}>
-        <ListGroup>
-          <ListGroupItemStyled>Model Builder</ListGroupItemStyled>
-          <ListGroupItemStyled>Publish Data Sources</ListGroupItemStyled>
-          <ListGroupItemStyled>Unpublish Data Sources</ListGroupItemStyled>
-          <ListGroupItemStyled>Admin</ListGroupItemStyled>
-          <ListGroupItemStyled>Sign Out</ListGroupItemStyled>
-        </ListGroup>
+        <Nav className="flex-column">
+          <Nav.Item>
+            <NavLinkStyled>Model Builder</NavLinkStyled>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLinkStyled>Link</NavLinkStyled>
+          </Nav.Item>
+          <NavDropdownStyled title="Published Data Sources" id="nav-dropdown">
+            <NavDropdownItemStyled>GDC</NavDropdownItemStyled>
+            <NavDropdownItemStyled>CTDC</NavDropdownItemStyled>
+            <NavDropdownItemStyled>ICDC</NavDropdownItemStyled>
+            <NavDropdownItemStyled>PCDC</NavDropdownItemStyled>
+          </NavDropdownStyled>
+          <NavDropdownStyled title="Unpublished Data Sources" id="nav-dropdown">
+            <NavDropdownItemStyled>Link 1</NavDropdownItemStyled>
+            <NavDropdownItemStyled>Link 2</NavDropdownItemStyled>
+          </NavDropdownStyled>
+          <Nav.Item>
+            <NavLinkStyled>User Management</NavLinkStyled>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLinkStyled>Sign Out</NavLinkStyled>
+          </Nav.Item>
+        </Nav>
+
         </Col>
         <Col sm={9}>
         <SectionContainer>
@@ -153,6 +274,18 @@ const Dashboard = (props) => {
             </div>
           </SectionHeader>
           <TableContainer>
+
+          <InputGroupStyled>
+            <FormControlStyled
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="btnGroupAddon"
+            />
+            <InputGroupTextStyled id="btnGroupAddon">
+              <InputGroupIcon icon={faSearch}/>
+            </InputGroupTextStyled>
+          </InputGroupStyled>
             <Table bordered>
               <thead>
                 <tr>
