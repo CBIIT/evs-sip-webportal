a0747505effeaa8054a0133496c787a5931bc3e8
Chao Zhang <chao.zhang3@nih.gov>
2021-10-25 15:44:29 -0400

    test 20211025 v2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware'); 
 
 module.exports = function(app) {
-    app.use('/private', createProxyMiddleware({
+    app.use('/dashboard', createProxyMiddleware({
         target: 'http://localhost:3000',
         changeOrigin: true
     }))
