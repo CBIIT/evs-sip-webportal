1b6a237490e2227669f44569639c55f773775090
Francisco M. Miranda <fmanuelos@gmail.com>
2023-12-19 09:58:06 -0500

    Update GraphcalView Icons Components

--- src/components/GraphicalView/DataDictionary/NodeCategories/SVGComponents/IconDefault.jsx
+++ src/components/GraphicalView/DataDictionary/NodeCategories/SVGComponents/IconDefault.jsx
@@ -6,8 +6,8 @@ class IconDefault extends React.Component {
             <svg width="20px" height="21px" viewBox="0 0 20 21" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <title>Group 47</title>
                 <desc>Created with Sketch.</desc>
-                <g id="specs" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g id="dictionary_Graph" transform="translate(-316.000000, -6106.000000)" fill-rule="nonzero">
+                <g id="specs" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                    <g id="dictionary_Graph" transform="translate(-316.000000, -6106.000000)" fillRule="nonzero">
                         <g id="Group-47" transform="translate(316.000000, 6106.000000)">
                             <g id="icon_administrative" fill="#9B9B9B">
                                 <path d="M10,0.498854962 C15.5225192,0.498854962 20,4.97633579 20,10.498855 C20,16.0207313 15.5225192,20.498855 10,20.498855 C4.47748082,20.498855 0,16.0213027 0,10.498855 C0,4.97640721 4.47683803,0.498854962 10,0.498854962 Z" id="Shape"></path>
--- src/components/GraphicalView/DataDictionary/NodeCategories/SVGComponents/IconMetadata.jsx
+++ src/components/GraphicalView/DataDictionary/NodeCategories/SVGComponents/IconMetadata.jsx
@@ -7,9 +7,9 @@ class IconMetadata extends React.Component {
             <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <title>icon_metadata</title>
                 <desc>Created with Sketch.</desc>
-                <g id="icon_metadata" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <path d="M10,0 C15.5225192,0 20,4.47748082 20,10 C20,15.5218764 15.5225192,20 10,20 C4.47748082,20 0,15.5224478 0,10 C0,4.47755224 4.47683803,0 10,0 Z" id="Shape" fill={fill} fill-rule="nonzero"></path>
-                    <g id="Group-9" transform="translate(4.500000, 6.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                <g id="icon_metadata" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                    <path d="M10,0 C15.5225192,0 20,4.47748082 20,10 C20,15.5218764 15.5225192,20 10,20 C4.47748082,20 0,15.5224478 0,10 C0,4.47755224 4.47683803,0 10,0 Z" id="Shape" fill={fill} fillRule="nonzero"></path>
+                    <g id="Group-9" transform="translate(4.500000, 6.000000)" fill="#FFFFFF" fillRule="nonzero">
                         <rect id="Rectangle-3" x="0" y="0" width="5" height="2" rx="0.5"></rect>
                         <path d="M6.5,0 L10.5,0 C10.7761424,-5.07265313e-17 11,0.223857625 11,0.5 L11,1.5 C11,1.77614237 10.7761424,2 10.5,2 L6.5,2 C6.22385763,2 6,1.77614237 6,1.5 L6,0.5 C6,0.223857625 6.22385763,5.07265313e-17 6.5,0 Z" id="Rectangle-3"></path>
                         <rect id="Rectangle-3" x="0" y="3" width="5" height="2" rx="0.5"></rect>
