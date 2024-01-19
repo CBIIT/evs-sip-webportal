edbfcd561743281c6a60a6b210edb3255bab4214
Francisco M. Miranda <fmanuelos@gmail.com>
2024-01-19 11:23:45 -0500

    Add eslint and prettier config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint'
 
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslint(),
-    basicSsl()
-  ],
+  plugins: [react(), eslint(), basicSsl()],
   base: `/evssip`,
   build: {
     outDir: './build',
