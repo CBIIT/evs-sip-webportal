7d687b036198fd91a08a7c5eb9db857d91f63bf8
Ben Chen <benchen4998@gmail.com>
2024-01-22 11:39:47 -0500

    fix pcdc styles

--- src/pages/DataSources/PCDCSource.jsx
+++ src/pages/DataSources/PCDCSource.jsx
@@ -1,135 +1,10 @@
 import { useEffect } from 'react'
-import styled from 'styled-components'
+import styles from './index.module.css';
 import { Button, Table, Pagination, InputGroup, FormControl, Tabs, Tab } from 'react-bootstrap'
 import { SearchIcon, AngleDownIcon, EditIcon, TimesIcon } from '../../components/ui/icons/Icons'
 
 import BatchUpdateModal from '../../components/Modals/BatchUpdateModal'
 import DashboardContainer from '../../components/DashboardContainer/DashboardContainer'
-
-const SectionHeader = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-`
-
-const SectionContainer = styled.div`
-  padding: 1rem;
-  background-color: #f7fbff;
-  border: solid #e4e8ed 1px;
-`
-
-const TableContainer = styled.div`
-  background-color: #fff;
-  padding: 1rem;
-  border: solid #e4e8ed 1px;
-`
-
-const SectionTitle = styled.h2`
-  font-family: 'Raleway-Medium', sans-serif;
-  font-weight: 500;
-  color: #042a68;
-  width: fit-content;
-`
-
-const PaginationContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: right;
-
-  && > .pagination {
-    margin-bottom: 0;
-  }
-`
-
-const ButtonStyled = styled(Button)`
-  font-size: 0.87rem;
-  border-radius: 1rem;
-  font-weight: 600;
-  margin-bottom: 0.3rem;
-
-  &&:hover,
-  &&:focus {
-    background-color: #6fc0d9;
-    border-color: #34859d;
-  }
-`
-
-const ButtonBlue = styled(ButtonStyled)`
-  background-color: #6fc0d9;
-  border-color: #34859d;
-
-  &&:hover,
-  &&:focus {
-    background-color: #5b9baf;
-  }
-`
-
-const ButtonGray = styled(ButtonStyled)`
-  background-color: #858d8f;
-  border-color: #424242;
-
-  &&:hover,
-  &&:focus {
-    background-color: #63696b;
-  }
-`
-
-const ButtonDarkBlue = styled(ButtonStyled)`
-  background-color: #34859d;
-  border-color: #34859d;
-
-  &&:hover,
-  &&:focus {
-    background-color: #245b6b;
-  }
-`
-
-const InputGroupStyled = styled(InputGroup)`
-  margin-bottom: 1rem;
-  max-width: 20rem;
-`
-
-const InputGroupTextStyled = styled(InputGroup.Text)`
-  position: relative;
-  left: -2.5rem;
-  z-index: 3;
-  background-color: transparent;
-  border: none;
-
-  && > .form-control {
-    border-radius: 0.25rem;
-  }
-`
-
-const FormControlStyled = styled(FormControl)`
-  border-radius: 1rem !important;
-  padding-right: 2rem;
-`
-
-const TableUl = styled.ul`
-  padding-left: 15px;
-  list-style: none;
-`
-
-const TableLi = styled.li`
-  position: relative;
-  word-wrap: break-word;
-`
-
-const SpanIcon = styled.span`
-  left: -1.1rem;
-  top: 0.3rem;
-  position: absolute;
-  width: 1rem;
-  line-height: inherit;
-  color: var(--checkbox-green);
-  transform: rotate(45deg);
-`
-
-const ActionLink = styled.a`
-  margin-left: 0.5rem;
-  font-size: 1.1rem;
-`
 
 const PCDCSource = (props) => {
   useEffect(() => {
@@ -138,21 +13,21 @@ const PCDCSource = (props) => {
 
   return (
     <DashboardContainer>
-      <SectionContainer>
-        <SectionHeader>
-          <SectionTitle>Pedriactic Cancer Data Model</SectionTitle>
+      <div className={styles['section-container']}>
+        <div className={styles['section-header']}>
+          <h2 className={styles['section-title']}>Pedriactic Cancer Data Model</h2>
           <div>
-            <ButtonBlue>Graphic View</ButtonBlue> <ButtonGray>Model Update</ButtonGray> <br />
-            <BatchUpdateModal /> <ButtonDarkBlue>Change Report</ButtonDarkBlue>{' '}
+            <Button className={styles['button-blue']}>Graphic View</Button> <Button className={styles['button-gray']}>Model Update</Button> <br />
+            <BatchUpdateModal /> <Button className={styles['button-dark-blue']}>Change Report</Button>{' '}
           </div>
-        </SectionHeader>
-        <TableContainer>
-          <InputGroupStyled>
-            <FormControlStyled type="text" placeholder="Search" aria-label="Search" aria-describedby="btnGroupAddon" />
-            <InputGroupTextStyled id="btnGroupAddon">
+        </div>
+        <div className={styles['table-container']}>
+          <InputGroup className={styles['input-group-styled']}>
+            <FormControl className={styles['form-control-styled']} type="text" placeholder="Search" aria-label="Search" aria-describedby="btnGroupAddon" />
+            <InputGroup.Text className={styles['input-group-text-styled']} id="btnGroupAddon">
               <SearchIcon />
-            </InputGroupTextStyled>
-          </InputGroupStyled>
+            </InputGroup.Text>
+          </InputGroup>
           <Tabs defaultActiveKey="values" id="uncontrolled-tab-example">
             <Tab eventKey="values" title="Values">
               <div>
@@ -169,152 +44,152 @@ const PCDCSource = (props) => {
                     <tr>
                       <td rowSpan="3">
                         {'biospecimen'}
-                        <TableUl>
-                          <TableLi>
-                            <SpanIcon>
+                        <ul className={styles['table-ul']}>
+                          <li className={styles['table-li']}>
+                            <span className={styles['span-icon']}>
                               <AngleDownIcon />
-                            </SpanIcon>
+                            </span>
                             {'read_group'}
-                            <TableUl>
-                              <TableLi>
-                                <SpanIcon>
+                            <ul className={styles['table-ul']}>
+                              <li className={styles['table-li']}>
+                                <span className={styles['span-icon']}>
                                   <AngleDownIcon />
-                                </SpanIcon>
+                                </span>
                                 {'chipseq_antibody'}
-                              </TableLi>
-                            </TableUl>
-                          </TableLi>
-                        </TableUl>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
                       </td>
                       <td>Not allowed to collect</td>
                       <td>C141478</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td>Unknown</td>
                       <td>C17998</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td>Not Reported</td>
                       <td>C43234</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td rowSpan="6">
                         {'biospecimen'}
-                        <TableUl>
-                          <TableLi>
-                            <SpanIcon>
+                        <ul className={styles['table-ul']}>
+                          <li className={styles['table-li']}>
+                            <span className={styles['span-icon']}>
                               <AngleDownIcon />
-                            </SpanIcon>
+                            </span>
                             {'read_group'}
-                            <TableUl>
-                              <TableLi>
-                                <SpanIcon>
+                            <ul className={styles['table-ul']}>
+                              <li className={styles['table-li']}>
+                                <span className={styles['span-icon']}>
                                   <AngleDownIcon />
-                                </SpanIcon>
+                                </span>
                                 {'chipseq_antibody'}
-                              </TableLi>
-                            </TableUl>
-                          </TableLi>
-                        </TableUl>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
                       </td>
                       <td>H3K4me1</td>
                       <td>C141478</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr style={{ backgroundColor: '#fff496' }}>
                       <td>Input Control</td>
                       <td>C17998</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td>H3K27ac</td>
                       <td>C43234</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']}ink href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr style={{ backgroundColor: '#fff496' }}>
                       <td>H3K27ac</td>
                       <td>C43234</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']}nLink href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td>H3K27ac</td>
                       <td>C43234</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                     <tr>
                       <td>H3K27me3</td>
                       <td>C43234</td>
                       <td>
-                        <ActionLink href="/#" aria-label="edit">
+                        <a className={styles['action-link']} href="/#" aria-label="edit">
                           <EditIcon />
-                        </ActionLink>
-                        <ActionLink href="/#" aria-label="edit">
+                        </a>
+                        <a className={styles['action-link']}Link href="/#" aria-label="edit">
                           <TimesIcon />
-                        </ActionLink>
+                        </a>
                       </td>
                     </tr>
                   </tbody>
                 </Table>
-                <PaginationContainer>
+                <div className={styles['pagination-container']}>
                   <Pagination>
                     <Pagination.First />
                     <Pagination.Item active>{1}</Pagination.Item>
@@ -324,7 +199,7 @@ const PCDCSource = (props) => {
                     <Pagination.Item>{5}</Pagination.Item>
                     <Pagination.Last />
                   </Pagination>
-                </PaginationContainer>
+                </div>
               </div>
             </Tab>
             <Tab eventKey="props" title="Properties">
@@ -334,8 +209,8 @@ const PCDCSource = (props) => {
               <p>test 3</p>
             </Tab>
           </Tabs>
-        </TableContainer>
-      </SectionContainer>
+        </div>
+      </div>
     </DashboardContainer>
   )
 }
